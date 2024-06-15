ac186734da749e1856a4c0e3078b312ffd610a0f
PauloMtz <pramosmtz@gmail.com>
2024-06-15 19:59:10 -0300

    API - Adicionando e utilizando ModelMapper

--- src/main/java/com/contatos/api/assembler/ContatoDtoAssembler.java
+++ src/main/java/com/contatos/api/assembler/ContatoDtoAssembler.java
@@ -3,18 +3,24 @@ package com.contatos.api.assembler;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.contatos.api.model.dto.ContatoDto;
-import com.contatos.api.model.dto.EnderecoDto;
+//import com.contatos.api.model.dto.EnderecoDto;
 import com.contatos.domain.model.Contato;
 
 @Component
 public class ContatoDtoAssembler {
+
+    @Autowired
+    private ModelMapper modelMapper;
     
     public ContatoDto convertToDto(Contato contato) {
 
-        EnderecoDto enderecoDto = new EnderecoDto();
+        // todo esse código é substituído pelo ModelMapper
+        /*EnderecoDto enderecoDto = new EnderecoDto();
         enderecoDto.setId(contato.getEndereco().getId());
         enderecoDto.setCidade(contato.getEndereco().getCidade());
         enderecoDto.setEstado(contato.getEndereco().getEstado());
@@ -25,7 +31,9 @@ public class ContatoDtoAssembler {
         contatoDto.setTelefone(contato.getTelefone());
         contatoDto.setEndereco(enderecoDto);
 
-        return contatoDto;
+        return contatoDto;*/
+
+        return modelMapper.map(contato, ContatoDto.class);
     }
 
     public List<ContatoDto> convertToCollectionDto(List<Contato> contatos) {
--- src/main/java/com/contatos/api/assembler/ContatoDtoDisassembler.java
+++ src/main/java/com/contatos/api/assembler/ContatoDtoDisassembler.java
@@ -1,16 +1,23 @@
 package com.contatos.api.assembler;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.contatos.api.model.inputDto.ContatoInputDto;
 import com.contatos.domain.model.Contato;
-import com.contatos.domain.model.Endereco;
+//import com.contatos.domain.model.Endereco;
 
 @Component
 public class ContatoDtoDisassembler {
+
+    @Autowired
+    private ModelMapper modelMapper;
     
     public Contato convertToDomainObject(ContatoInputDto inputDto) {
-        Contato contato = new Contato();
+
+        // todo esse código é substituído pelo ModelMapper
+        /*Contato contato = new Contato();
         contato.setNome(inputDto.getNome());
         contato.setCpf(inputDto.getCpf());
         contato.setEmail(inputDto.getEmail());
@@ -21,6 +28,8 @@ public class ContatoDtoDisassembler {
 
         contato.setEndereco(endereco);
 
-        return contato;
+        return contato;*/
+
+        return modelMapper.map(inputDto, Contato.class);
     }
 }
